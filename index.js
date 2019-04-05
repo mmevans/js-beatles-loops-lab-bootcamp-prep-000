63490f4ac75202eb7a6029b1268634665065b7d7
Michael Evans <mmevansjr@gmail.com>
2019-04-05 16:54:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  
+  for(var i = 0; i < musicians.length; i++) {
+    empty.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return empty;
+}
+
+function johnLennonFacts(facts) {
+  var results = [];
+  var i = 0;
+  while(i < facts.length) {
+    results.push(facts[i] + "!!!");
+    i++;
+  }
+  return results;
+}
